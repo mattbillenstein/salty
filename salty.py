08c9bc749aaf77b01006b470f028c199734d8b7f
Matt Billenstein <matt@vazor.com>
2022-09-21 18:53:27 -0700

    Rather, start with a new dict

--- salty.py
+++ salty.py
@@ -216,7 +216,8 @@ class SaltyServer(gevent.server.StreamServer, Reactor):
 
                     v['cluster'] = cluster
                     v['facts'] = self.facts[id]
-                    v['vars'] = dict(meta['envs'][v['env']])
+                    v['vars'] = {}
+                    v['vars'].update(meta['envs'][v['env']])
                     v['vars'].update(meta['clusters'][cluster])
                     hosts[id] = v
 
