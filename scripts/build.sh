e30a738d8e9129a25ea4995ce290eec16f9fbdff
Matt Billenstein <matt@vazor.com>
2023-06-22 05:36:56 +0000

    More ignores, fix nuitka build options

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,4 @@ SCRIPTPATH="$(pwd)"
 popd > /dev/null
 
 source $SCRIPTPATH/.venv/bin/activate
-nuitka3 --follow-imports --output-filename="salty.$(uname -s)-$(uname -m)" salty.py
+nuitka3 --standalone --onefile --output-filename="salty.$(uname -s)-$(uname -m)" salty.py
