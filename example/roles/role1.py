04ece8ba4eeb3c1a39c12e41c30b56946e259753
Matt Billenstein <matt@vazor.com>
2025-02-08 18:20:46 +0000

    Clean up example role a bit

--- example/roles/role1.py
+++ example/roles/role1.py
@@ -1,7 +1,7 @@
 # files are rooted in the 'files' dir
 x = copy('role1/afile.txt', '/etc/afile.txt')
 if x['changed']:
-    copy('role1/afile.txt', '/etc/afile1.txt')
+    shell('echo "/etc/afile.txt changed"')
 
 render('role1/atemplate.txt', '/etc/atemplate.txt', extra='some extra data')
 
@@ -10,8 +10,6 @@ for i in range(3):
   copy('role1/afile.txt', f'/etc/afile{i}.txt')
 
 # and you can run shell on the remote
-shell('''
-echo "The date is $(date)"
-''')
+shell('echo "The date is $(date)"')
 
 syncdir('foo', '/foo')
